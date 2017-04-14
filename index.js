ab4c2fda62558e5526b0bb6a621fa3929390d467
Guangyuan (Charlie) Yang <yzgyyang@outlook.com>
2017-04-14 08:07:30 -0400

    Add Procfile

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 
 // Routes
 app.get('/', function(req, res) {
-    res.send('Hi, I am a UWPC Rental Bot!')
+    res.send("Hi, I am a UWPC Rental Bot!")
 })
 
 // Facebook
