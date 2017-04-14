f29448562314a94fac65793a7ee5b4a2d8fab040
Guangyuan (Charlie) Yang <yzgyyang@outlook.com>
2017-04-14 07:45:12 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body=parser')
+const request = require('request')
+
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Allow to process the data
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// Routes
+app.get('/', function(req, res) {
+    res.send('Hi, I am a UWPC Rental Bot!')
+})
+
+// Facebook
+app.get('/webhook/', function(req, res) {
+    if (req.query['hub.verify_token'] === "uwpcishype") {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send("Wrong token!")
+})
+
